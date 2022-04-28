e023c5785710329f4a6bc04ec4544ca1927737bf
Yashwant Krishna Pagoti <pagotiyashwantkrishna@gmail.com>
2022-04-29 00:54:55 +0530

    date formatting is done

--- components/RideList.js
+++ components/RideList.js
@@ -1,20 +1,41 @@
-import { useState, useEffect } from 'react'
 import Image from 'next/image'
 import EmptyList from './EmptyList'
 import styles from '../styles/RideList.module.css'
 
 const RideList = ({ rides }) => {
-    const getStationPath = (ride) => {
+    const getStationPath = (path) => {
         var pathStr = ""
-        for (var i = 0; i < ride.station_path.length; i++) {
-            pathStr += ride.station_path[i]
+        for (var i = 0; i < path.length; i++) {
+            pathStr += path[i]
 
-            if (i < ride.station_path.length - 1) {
+            if (i < path.length - 1) {
                 pathStr += ", "
             }
         }
 
         return '[' + pathStr + ']'
+    }
+
+    const nth = (d) => {
+        if (d > 3 && d < 21) return 'th'
+        switch (d % 10) {
+            case 1: return "st"
+            case 2: return "nd"
+            case 3: return "rd"
+            default: return "th"
+        }
+    }
+
+    const getDate = (date) => {
+        let newDate = new Date(date)
+        const tdate = newDate.getDate()
+        const month = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'][newDate.getMonth()]
+        const year = newDate.getFullYear()
+
+        const hours = newDate.getHours()
+        const minutes = newDate.getMinutes()
+
+        return `${tdate}${nth(tdate)} ${month} ${year} ${hours}:${minutes}`
     }
 
     return (
@@ -29,8 +50,8 @@ const RideList = ({ rides }) => {
                             <div className={styles.cardContent}>
                                 <p>Ride Id : <span>{ride.id}</span></p>
                                 <p>Origin Station : <span>{ride.origin_station_code}</span></p>
-                                <p>station_path : <span>{getStationPath(ride)}</span></p>
-                                <p>Date : <span>{ride.date}</span></p>
+                                <p>station_path : <span>{getStationPath(ride.station_path)}</span></p>
+                                <p>Date : <span>{getDate(ride.date)}</span></p>
                                 <p>Distance : <span>{ride.distance}</span></p>
                             </div>
                         </div>
