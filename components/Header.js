611d5ba9ec9c5133f104130c56583fdaced67b67
Yashwant Krishna Pagoti <pagotiyashwantkrishna@gmail.com>
2022-04-29 10:39:29 +0530

    removed existing mediaquery

--- components/Header.js
+++ components/Header.js
@@ -1,16 +1,13 @@
 import Image from 'next/image'
 import styles from '../styles/Header.module.css'
 import cstyles from '../styles/Common.module.css'
-import MediaQuery from 'react-responsive'
 
 const Header = ({ user }) => {
 	return (
 		<div className={`${styles.header} ${cstyles.container}`}>
 			<div className={styles.name}>Rides API</div>
 			<div className={styles.user}>
-				<MediaQuery query='(min-width: 768px)'>
-					<div className={styles.name}>{user.name}</div>
-				</MediaQuery>
+				<div className={styles.name}>{user.name}</div>
 				<Image className={styles.logo} src={user.url} alt="user" width={44} height={44} />
 			</div>
 		</div>
